baa96cbcd0f11b59e68f422ac8189d92c9139505
Keyur Patel <patelkeyur2311@gmail.com>
2022-08-29 23:35:43 +0530

    All files added

---
+++ index.js
@@ -0,0 +1,47 @@
+let city = '';
+async function getWeatherdata() {
+    let api = 'http://api.openweathermap.org/data/2.5/weather?q='
+    city = document.querySelector('.cityName').value;
+    let apiKey = '&APPID=c6bafb8227a66ae45c8ac2b016e61b31&units=metric&lang=french';
+    let units = '&units=metric';
+
+    let url = api + city + apiKey + units;
+    try {
+        let response = await fetch(url);
+        if (response.status >= 200 && response.status <= 299) {
+            return response.json();
+        } else {
+            let html = '';
+            html += '<h4> oops, Sorry we don\'t have any weather information about this city.</h4>'
+            document.querySelector('.cityName').value = '';
+            document.getElementById('wcontent').innerHTML = html;
+            throw Error(response.statusText);
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+async function loadWeatherData() {
+    let data = await getWeatherdata();
+
+    let html = '';
+    let temperature = data.main.temp;
+    let humidity = data.main.humidity;
+    let windSpeed = data.wind.speed;
+    let windDeg = data.wind.deg;
+    let wdes = data.weather[0].description;
+    html += `<h4> Weather in ${city}</h4>
+    <h3>${temperature}<sup>o</sup>C
+    </h3>
+    <h5> Wind Speed :- ${windSpeed} km/h, ${windDeg}<sup>o</sup><h5>
+    <h5> Humidity :- ${humidity} %</h5>
+    <p class="wd"> Weather description :- ${wdes}  <p>
+    `;
+
+    document.querySelector('.cityName').value = '';
+    city = '';
+    document.getElementById('wcontent').innerHTML = html;
+
+}
